55b10d8a9b2adfbadb0faf6714121323e0211951
catse2000 <catse2000@yahoo.com>
2020-10-25 17:28:57 -0500

    inital application

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,15 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
